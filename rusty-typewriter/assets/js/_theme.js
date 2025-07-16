3f7dbde519ec6ac470f5cd382061cd4fdcffb575
squirrelhurl <aknocke@protonmail.com>
2025-07-16 16:25:55 -0500

    Add files via upload

---
+++ rusty-typewriter/assets/js/_theme.js
@@ -0,0 +1,38 @@
+(function() {
+    "use strict";
+
+    const getCachedTheme = () => {
+      const theme = document.body.getAttribute("color-scheme");
+      const cachedTheme = localStorage.getItem("color-scheme");
+      if (cachedTheme) {
+        return cachedTheme;
+      } else if (theme !== "dark" || theme !== "light") {
+        let preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        return preferDark ? "dark" : "light";
+      } else {
+        return theme
+      }
+    };
+
+    const toggle = (state) => state === "light" ? "dark" : "light";
+    const initTheme = (state) => {
+      document.documentElement.setAttribute("color-scheme", toggle(state));
+    };
+
+    const toggleTheme = () => {
+      const state = getCachedTheme();
+        localStorage.setItem("color-scheme", toggle(state));
+        initTheme(toggle(state));
+    };
+
+    window.addEventListener("DOMContentLoaded", () => {
+      initTheme(getCachedTheme());
+      requestAnimationFrame(() => document.body.classList.remove("notransition"))
+
+      const switcher = document.getElementById("theme-switcher");
+      switcher.addEventListener("click", e => {
+        e.preventDefault();
+        toggleTheme()
+      });
+    });
+})();
